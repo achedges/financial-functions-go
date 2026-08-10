adc507cb79b9b0cbc89bc3e4bfd737813ef2c4a8
Adam Hedges <achedges@gmail.com>
2026-08-09 20:30:46 -0500

    Implemented RSI

---
+++ window/rsi.go
@@ -0,0 +1,75 @@
+package window
+
+import (
+	"github.com/achedges/financial-core-go/pricebar"
+	"github.com/shopspring/decimal"
+)
+
+// Relative Strength Index implementation
+
+type RelativeStrengthIndex struct {
+	period           int
+	barsWindow       []pricebar.PriceBar
+	relativeStrength decimal.Decimal
+	rsi              decimal.Decimal
+}
+
+func NewRelativeStrengthIndex(period int, bars []pricebar.PriceBar) *RelativeStrengthIndex {
+	rsi := RelativeStrengthIndex{
+		period:     period,
+		barsWindow: bars,
+	}
+
+	if len(rsi.barsWindow) == period {
+		rsi.Calculate()
+	}
+
+	return &rsi
+}
+
+func (rsi *RelativeStrengthIndex) RelativeStrength() decimal.Decimal {
+	return rsi.relativeStrength
+}
+
+func (rsi *RelativeStrengthIndex) RSI() decimal.Decimal {
+	return rsi.rsi
+}
+
+func (rsi *RelativeStrengthIndex) Calculate() {
+	// This is a bit different than the original calculation.
+	// For a 14-period measurement, Wilder's book includes a 15th price before calculating.
+	// Since I'm not planning to use a period of 14 it seem unnecessary to adjust the backfill/period logic to account for this.
+
+	gains := decimal.Zero
+	losses := decimal.Zero
+
+	for i := 1; i < len(rsi.barsWindow); i++ {
+		curr := rsi.barsWindow[i]
+		prev := rsi.barsWindow[i-1]
+		if curr.Close > prev.Close {
+			gains = gains.Add(decimal.NewFromFloat(curr.Close - prev.Close))
+		} else if curr.Close < prev.Close {
+			losses = losses.Add(decimal.NewFromFloat(prev.Close - curr.Close))
+		}
+	}
+
+	n := decimal.NewFromInt32(int32(len(rsi.barsWindow)))
+	onehundred := decimal.NewFromInt32(100)
+	one := decimal.NewFromInt32(1)
+
+	avgGains := gains.Div(n)
+	avgLosses := losses.Div(n)
+
+	rsi.relativeStrength = avgGains.Div(avgLosses)
+	rsi.rsi = onehundred.Sub(onehundred.Div(rsi.relativeStrength.Add(one)))
+}
+
+func (rsi *RelativeStrengthIndex) Slide(bar pricebar.PriceBar) {
+	if len(rsi.barsWindow) == rsi.period {
+		rsi.barsWindow = rsi.barsWindow[1:] // remove first element
+	}
+	rsi.barsWindow = append(rsi.barsWindow, bar)
+	if len(rsi.barsWindow) == rsi.period {
+		rsi.Calculate()
+	}
+}
---
+++ window/rsi_test.go
@@ -0,0 +1,34 @@
+package window_test
+
+import (
+	"financial/functions/window"
+	"testing"
+
+	"github.com/achedges/financial-core-go/pricebar"
+	"github.com/achedges/go-assertions"
+)
+
+func TestRelativeStrengthIndex_Slide(t *testing.T) {
+	bars := []pricebar.PriceBar{
+		*pricebar.New(pricebar.Config{BasisPrice: 54.80}),
+		*pricebar.New(pricebar.Config{BasisPrice: 56.80}),
+		*pricebar.New(pricebar.Config{BasisPrice: 57.85}),
+		*pricebar.New(pricebar.Config{BasisPrice: 59.85}),
+		*pricebar.New(pricebar.Config{BasisPrice: 60.57}),
+		*pricebar.New(pricebar.Config{BasisPrice: 61.10}),
+		*pricebar.New(pricebar.Config{BasisPrice: 62.17}),
+		*pricebar.New(pricebar.Config{BasisPrice: 60.60}),
+		*pricebar.New(pricebar.Config{BasisPrice: 62.35}),
+		*pricebar.New(pricebar.Config{BasisPrice: 62.15}),
+		*pricebar.New(pricebar.Config{BasisPrice: 62.35}),
+		*pricebar.New(pricebar.Config{BasisPrice: 61.45}),
+		*pricebar.New(pricebar.Config{BasisPrice: 62.80}),
+		*pricebar.New(pricebar.Config{BasisPrice: 61.37}),
+	}
+
+	rsi := window.NewRelativeStrengthIndex(len(bars), bars)
+	rsi.Slide(*pricebar.New(pricebar.Config{BasisPrice: 62.50}))
+
+	assertions.CloseEnough(2.39, rsi.RelativeStrength().InexactFloat64(), 0.001, t)
+	assertions.CloseEnough(70.50, rsi.RSI().InexactFloat64(), 0.01, t)
+}
