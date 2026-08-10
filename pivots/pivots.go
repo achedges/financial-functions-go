9b30715f0e7f3441d124be30c356d2ec64be548a
Adam Hedges <achedges@gmail.com>
2026-08-10 16:25:25 -0500

    Removed unnecessary pointer references from Pivots implementation

--- pivots/pivots.go
+++ pivots/pivots.go
@@ -8,16 +8,16 @@ const DefaultDispersionSpan = 4
 const DefaultConsolidationSpan = 4
 
 type Params[T any] struct {
-	Values            *[]T
+	Values            []T
 	ComparisonFunc    func(l T, r T) bool
 	DifferenceFunc    func(l T, r T) float64
 	ConsolidationSpan int
 	DispersionSpan    int
 }
 
-func FindAllIndexes[T any](values *[]T, comparison func(a T, b T) bool) *[]int {
+func FindAllIndexes[T any](values []T, comparison func(a T, b T) bool) []int {
 	// Find all elements where the provided comparison lambda is true for all non-null neighbors, and return their indices
-	n := len(*values)
+	n := len(values)
 	pivots := make([]int, 0, n/2) // start with half the size of the input
 
 	var c *T
@@ -25,22 +25,22 @@ func FindAllIndexes[T any](values *[]T, comparison func(a T, b T) bool) *[]int {
 	var r *T
 
 	for i := 0; i < n; i++ {
-		c = &(*values)[i]
+		c = &values[i]
 
 		if i == 0 {
 			l = nil
 		} else {
-			l = &(*values)[i-1]
+			l = &values[i-1]
 		}
 
 		if i == n-1 {
 			r = nil
 		} else {
-			r = &(*values)[i+1]
+			r = &values[i+1]
 		}
 
 		if l == nil && r == nil {
-			return &pivots
+			return pivots
 		} else if l != nil && r != nil {
 			if comparison(*c, *l) && comparison(*c, *r) {
 				pivots = append(pivots, i)
@@ -52,27 +52,27 @@ func FindAllIndexes[T any](values *[]T, comparison func(a T, b T) bool) *[]int {
 		}
 	}
 
-	return &pivots
+	return pivots
 }
 
-func CalculateDispersions[T any](values *[]T, pivotIndexes *[]int, span int, difference func(a T, b T) float64) *[]float64 {
+func CalculateDispersions[T any](values []T, pivotIndexes []int, span int, difference func(a T, b T) float64) []float64 {
 	// Given a list of values, and a corresponding list of pivots (from getPivots()), find the dispersion of each pivot point
 	// by calculating the maximum difference between that pivot point and the neighboring elements, up to some maximum span.
-	n := len(*values)
-	dispersions := make([]float64, len(*pivotIndexes))
+	n := len(values)
+	dispersions := make([]float64, len(pivotIndexes))
 
-	for i, pivot := range *pivotIndexes {
+	for i, pivot := range pivotIndexes {
 		dispersion := 0.0
-		peak := (*values)[pivot]
+		peak := values[pivot]
 		for s := 1; s <= span; s++ {
 			leftspan := 0.0
 			rightspan := 0.0
 
 			if pivot-s >= 0 {
-				leftspan = difference(peak, (*values)[pivot-s])
+				leftspan = difference(peak, values[pivot-s])
 			}
 			if pivot+s < n {
-				rightspan = difference(peak, (*values)[pivot+s])
+				rightspan = difference(peak, values[pivot+s])
 			}
 
 			maxspan := math.Max(leftspan, rightspan)
@@ -84,25 +84,25 @@ func CalculateDispersions[T any](values *[]T, pivotIndexes *[]int, span int, dif
 		dispersions[i] = dispersion
 	}
 
-	return &dispersions
+	return dispersions
 }
 
-func Consolidate(pivotIndexes *[]int, dispersions *[]float64, span int) *[]int {
+func Consolidate(pivotIndexes []int, dispersions []float64, span int) []int {
 	// Given a list of pivot indices (from getPivots()) and dispersions (from getDispersions()), produce a consolidated
 	// list of pivot indices by collapsing neighboring pivot points into the pivot point with the highest dispersion,
 	// up to some maximum span.
-	n := len(*pivotIndexes)
+	n := len(pivotIndexes)
 	results := make([]int, 0, n)
-	if len(*pivotIndexes) != len(*dispersions) {
-		return &results
+	if len(pivotIndexes) != len(dispersions) {
+		return results
 	}
 
 	consolidated := make([]int, n)
-	copy(consolidated, *pivotIndexes)
+	copy(consolidated, pivotIndexes)
 
 	for i := 0; i < n; i++ {
 		for j := i + 1; j < n; j++ {
-			if (*pivotIndexes)[j]-(*pivotIndexes)[i] > span {
+			if pivotIndexes[j]-pivotIndexes[i] > span {
 				break
 			}
 
@@ -110,7 +110,7 @@ func Consolidate(pivotIndexes *[]int, dispersions *[]float64, span int) *[]int {
 				continue
 			}
 
-			if (*dispersions)[i] > (*dispersions)[j] && consolidated[i] != -1 {
+			if dispersions[i] > dispersions[j] && consolidated[i] != -1 {
 				consolidated[j] = -1
 			} else if consolidated[j] != -1 {
 				consolidated[i] = -1
@@ -126,22 +126,22 @@ func Consolidate(pivotIndexes *[]int, dispersions *[]float64, span int) *[]int {
 		}
 	}
 
-	return &results
+	return results
 }
 
-//func Get[T any](params Params[T]) *[]int {
-//	//if params.Values == nil {
-//	//	return make(*[]int, 0)
-//	//}
-//
-//	if params.DispersionSpan == 0 {
-//		params.DispersionSpan = len(*params.Values) / DefaultDispersionSpan
-//	}
-//	if params.ConsolidationSpan == 0 {
-//		params.ConsolidationSpan = len(*params.Values) / DefaultConsolidationSpan
-//	}
-//
-//	rawPivots := FindAllIndexes(params.Values, params.ComparisonFunc)
-//	dispersions := CalculateDispersions(params.Values, rawPivots, params.DispersionSpan, params.DifferenceFunc)
-//
-//}
+func Get[T any](params Params[T]) []int {
+	if params.Values == nil {
+		return make([]int, 0)
+	}
+
+	if params.DispersionSpan == 0 {
+		params.DispersionSpan = len(params.Values) / DefaultDispersionSpan
+	}
+	if params.ConsolidationSpan == 0 {
+		params.ConsolidationSpan = len(params.Values) / DefaultConsolidationSpan
+	}
+
+	rawPivots := FindAllIndexes(params.Values, params.ComparisonFunc)
+	dispersions := CalculateDispersions(params.Values, rawPivots, params.DispersionSpan, params.DifferenceFunc)
+	return Consolidate(rawPivots, dispersions, params.ConsolidationSpan)
+}
--- pivots/pivots_test.go
+++ pivots/pivots_test.go
@@ -8,55 +8,55 @@ import (
 )
 
 func TestPivots_FindAllIndexes(t *testing.T) {
-	highPivots := pivots.FindAllIndexes(&pivotPrices1, func(l float64, r float64) bool { return l >= r })
-	assertions.False(len(*highPivots) == 0, t)
-	assertions.EqualInts(len(expectedHighPivots1), len(*highPivots), t)
+	highPivots := pivots.FindAllIndexes(pivotPrices1, func(l float64, r float64) bool { return l >= r })
+	assertions.False(len(highPivots) == 0, t)
+	assertions.EqualInts(len(expectedHighPivots1), len(highPivots), t)
 	for i, exp := range expectedHighPivots1 {
-		assertions.EqualInts(exp, (*highPivots)[i], t)
+		assertions.EqualInts(exp, highPivots[i], t)
 	}
 
-	lowPivots := pivots.FindAllIndexes(&pivotPrices1, func(l float64, r float64) bool { return l <= r })
-	assertions.False(len(*lowPivots) == 0, t)
-	assertions.EqualInts(len(expectedLowPivots1), len(*lowPivots), t)
+	lowPivots := pivots.FindAllIndexes(pivotPrices1, func(l float64, r float64) bool { return l <= r })
+	assertions.False(len(lowPivots) == 0, t)
+	assertions.EqualInts(len(expectedLowPivots1), len(lowPivots), t)
 	for i, exp := range expectedLowPivots1 {
-		assertions.EqualInts(exp, (*lowPivots)[i], t)
+		assertions.EqualInts(exp, lowPivots[i], t)
 	}
 
 	// second example from 20260428 0246-0345 (highs only)
-	highPivots2 := pivots.FindAllIndexes(&pivotPrices2, func(l float64, r float64) bool { return l >= r })
-	assertions.False(len(*highPivots2) == 0, t)
-	assertions.EqualInts(len(expectedHighPivots2), len(*highPivots2), t)
+	highPivots2 := pivots.FindAllIndexes(pivotPrices2, func(l float64, r float64) bool { return l >= r })
+	assertions.False(len(highPivots2) == 0, t)
+	assertions.EqualInts(len(expectedHighPivots2), len(highPivots2), t)
 	for i, exp := range expectedHighPivots2 {
-		assertions.EqualInts(exp, (*highPivots2)[i], t)
+		assertions.EqualInts(exp, highPivots2[i], t)
 	}
 }
 
 func TestPivots_CalculateDispersions(t *testing.T) {
 	span := len(pivotPrices1) / pivots.DefaultDispersionSpan
 
-	highPivots := pivots.FindAllIndexes(&pivotPrices1, func(l float64, r float64) bool { return l >= r })
-	highDispersions := pivots.CalculateDispersions(&pivotPrices1, highPivots, span, func(l float64, r float64) float64 { return l - r })
-	assertions.False(len(*highDispersions) == 0, t)
-	assertions.EqualInts(len(expectedHighDispersions1), len(*highDispersions), t)
+	highPivots := pivots.FindAllIndexes(pivotPrices1, func(l float64, r float64) bool { return l >= r })
+	highDispersions := pivots.CalculateDispersions(pivotPrices1, highPivots, span, func(l float64, r float64) float64 { return l - r })
+	assertions.False(len(highDispersions) == 0, t)
+	assertions.EqualInts(len(expectedHighDispersions1), len(highDispersions), t)
 	for i, exp := range expectedHighDispersions1 {
-		assertions.CloseEnough(exp, (*highDispersions)[i], 0.00001, t)
+		assertions.CloseEnough(exp, highDispersions[i], 0.00001, t)
 	}
 
-	lowPivots := pivots.FindAllIndexes(&pivotPrices1, func(l float64, r float64) bool { return l <= r })
-	lowDispersions := pivots.CalculateDispersions(&pivotPrices1, lowPivots, span, func(l float64, r float64) float64 { return r - l })
-	assertions.False(len(*lowDispersions) == 0, t)
-	assertions.EqualInts(len(expectedLowDispersions1), len(*lowDispersions), t)
+	lowPivots := pivots.FindAllIndexes(pivotPrices1, func(l float64, r float64) bool { return l <= r })
+	lowDispersions := pivots.CalculateDispersions(pivotPrices1, lowPivots, span, func(l float64, r float64) float64 { return r - l })
+	assertions.False(len(lowDispersions) == 0, t)
+	assertions.EqualInts(len(expectedLowDispersions1), len(lowDispersions), t)
 	for i, exp := range expectedLowDispersions1 {
-		assertions.CloseEnough(exp, (*lowDispersions)[i], 0.00001, t)
+		assertions.CloseEnough(exp, lowDispersions[i], 0.00001, t)
 	}
 
 	// second example from 20260428 0246-0345 (highs only)
-	highPivots2 := pivots.FindAllIndexes(&pivotPrices2, func(l float64, r float64) bool { return l >= r })
-	highDispersions2 := pivots.CalculateDispersions(&pivotPrices2, highPivots2, span, func(l float64, r float64) float64 { return l - r })
-	assertions.False(len(*highDispersions2) == 0, t)
-	assertions.EqualInts(len(expectedHighDispersions2), len(*highDispersions2), t)
+	highPivots2 := pivots.FindAllIndexes(pivotPrices2, func(l float64, r float64) bool { return l >= r })
+	highDispersions2 := pivots.CalculateDispersions(pivotPrices2, highPivots2, span, func(l float64, r float64) float64 { return l - r })
+	assertions.False(len(highDispersions2) == 0, t)
+	assertions.EqualInts(len(expectedHighDispersions2), len(highDispersions2), t)
 	for i, exp := range expectedHighDispersions2 {
-		assertions.CloseEnough(exp, (*highDispersions2)[i], 0.00001, t)
+		assertions.CloseEnough(exp, highDispersions2[i], 0.00001, t)
 	}
 }
 
@@ -64,30 +64,56 @@ func TestPivots_Consolidate(t *testing.T) {
 	dSpan := len(pivotPrices1) / pivots.DefaultDispersionSpan
 	cSpan := len(pivotPrices1) / pivots.DefaultConsolidationSpan
 
-	highPivots := pivots.FindAllIndexes(&pivotPrices1, func(l float64, r float64) bool { return l >= r })
-	highDispersions := pivots.CalculateDispersions(&pivotPrices1, highPivots, dSpan, func(l float64, r float64) float64 { return l - r })
+	highPivots := pivots.FindAllIndexes(pivotPrices1, func(l float64, r float64) bool { return l >= r })
+	highDispersions := pivots.CalculateDispersions(pivotPrices1, highPivots, dSpan, func(l float64, r float64) float64 { return l - r })
 
-	lowPivots := pivots.FindAllIndexes(&pivotPrices1, func(l float64, r float64) bool { return l <= r })
-	lowDispersions := pivots.CalculateDispersions(&pivotPrices1, lowPivots, dSpan, func(l float64, r float64) float64 { return r - l })
+	lowPivots := pivots.FindAllIndexes(pivotPrices1, func(l float64, r float64) bool { return l <= r })
+	lowDispersions := pivots.CalculateDispersions(pivotPrices1, lowPivots, dSpan, func(l float64, r float64) float64 { return r - l })
 
 	consolidatedHighs := pivots.Consolidate(highPivots, highDispersions, cSpan)
-	assertions.EqualInts(len(expectedHighConsolidations1), len(*consolidatedHighs), t)
+	assertions.EqualInts(len(expectedHighConsolidations1), len(consolidatedHighs), t)
 	for i, exp := range expectedHighConsolidations1 {
-		assertions.EqualInts(exp, (*consolidatedHighs)[i], t)
+		assertions.EqualInts(exp, consolidatedHighs[i], t)
 	}
 
 	consolidatedLows := pivots.Consolidate(lowPivots, lowDispersions, cSpan)
-	assertions.EqualInts(len(expectedLowConsolidations1), len(*consolidatedLows), t)
+	assertions.EqualInts(len(expectedLowConsolidations1), len(consolidatedLows), t)
 	for i, exp := range expectedLowConsolidations1 {
-		assertions.EqualInts(exp, (*consolidatedLows)[i], t)
+		assertions.EqualInts(exp, (consolidatedLows)[i], t)
 	}
 
 	// second example from 20260428 0246-0345 (highs only)
-	highPivots2 := pivots.FindAllIndexes(&pivotPrices2, func(l float64, r float64) bool { return l >= r })
-	highDispersions2 := pivots.CalculateDispersions(&pivotPrices2, highPivots2, dSpan, func(l float64, r float64) float64 { return l - r })
+	highPivots2 := pivots.FindAllIndexes(pivotPrices2, func(l float64, r float64) bool { return l >= r })
+	highDispersions2 := pivots.CalculateDispersions(pivotPrices2, highPivots2, dSpan, func(l float64, r float64) float64 { return l - r })
 	consolidatedHighs2 := pivots.Consolidate(highPivots2, highDispersions2, cSpan)
-	assertions.EqualInts(len(expectedHighConsolidations2), len(*consolidatedHighs2), t)
+	assertions.EqualInts(len(expectedHighConsolidations2), len(consolidatedHighs2), t)
 	for i, exp := range expectedHighConsolidations2 {
-		assertions.EqualInts(exp, (*consolidatedHighs2)[i], t)
+		assertions.EqualInts(exp, consolidatedHighs2[i], t)
+	}
+}
+
+func TestPivots_Get(t *testing.T) {
+	expectedPivots := []int{1, 7, 12}
+
+	defaultPivots := pivots.Get(pivots.Params[float64]{
+		Values:         data,
+		ComparisonFunc: func(l float64, r float64) bool { return l > r },
+		DifferenceFunc: func(l float64, r float64) float64 { return l - r },
+	})
+	assertions.EqualInts(len(expectedPivots), len(defaultPivots), t)
+	for i, exp := range expectedPivots {
+		assertions.EqualInts(exp, defaultPivots[i], t)
+	}
+
+	overridePivots := pivots.Get(pivots.Params[float64]{
+		Values:            data,
+		ComparisonFunc:    func(l float64, r float64) bool { return l > r },
+		DifferenceFunc:    func(l float64, r float64) float64 { return l - r },
+		DispersionSpan:    2,
+		ConsolidationSpan: 3,
+	})
+	assertions.EqualInts(len(expectedPivots), len(overridePivots), t)
+	for i, exp := range expectedPivots {
+		assertions.EqualInts(exp, overridePivots[i], t)
 	}
 }
